eb2883ae25de2e260907c520d39962f7aa560083
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-07-12 14:59:09 +0300

    add CS solution

---
+++ CS/DashboardDesigner_ControlAccess/DesignerForm1.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.XtraCharts;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraPivotGrid;
+using System.Drawing;
+
+namespace DashboardDesigner_ControlAccess
+{
+    public partial class DesignerForm1: DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public DesignerForm1() {
+            InitializeComponent();
+            dashboardDesigner.CreateRibbon();
+            dashboardDesigner.LoadDashboard(@"..\..\Dashboards\dashboard1.xml");
+        }
+
+        private void dashboardDesigner_DashboardItemControlCreated(object sender, DevExpress.DashboardWin.DashboardItemControlEventArgs e) {
+            if(e.DashboardItemName == "pivotDashboardItem1") {
+                PivotGridControl pivotGridControl = e.PivotGridControl;
+                pivotGridControl.CustomCellValue += PivotGridControl_CustomCellValue; ;
+            }
+        }
+
+        private void PivotGridControl_CustomCellValue(object sender, PivotCellValueEventArgs e) {
+            if(e.Value != null && (double)e.Value < 200000)
+                e.Value = 0;
+        }
+
+        private void dashboardDesigner_DashboardItemControlUpdated(object sender, DevExpress.DashboardWin.DashboardItemControlEventArgs e) {
+            if(e.DashboardItemName == "gridDashboardItem1") {
+                GridView gridView = e.GridControl.MainView as GridView;
+                gridView.Appearance.Row.Font = new Font("Arial", 10);
+            }
+            if(e.DashboardItemName == "chartDashboardItem1") {
+                ChartControl chartControl = e.ChartControl;
+                ((XYDiagram)chartControl.Diagram).Panes[0].BackColor = Color.AliceBlue;
+            }
+        }
+
+        private void dashboardDesigner_DashboardItemBeforeControlDisposed(object sender, DevExpress.DashboardWin.DashboardItemControlEventArgs e) {
+            if(e.DashboardItemName == "pivotDashboardItem1") {
+                PivotGridControl pivotGridControl = e.PivotGridControl;
+                pivotGridControl.CustomCellValue -= PivotGridControl_CustomCellValue;
+            }
+        }
+    }
+}
