2ec741a6c66647c62c2b8236cca4f7ee371eef72
Natalia Kazakova <natalia.kazakova@devexpress.com>
2022-12-26 13:51:16 +0400

    add gauge

--- CS/DashboardDesigner_ControlAccess/DesignerForm1.cs
+++ CS/DashboardDesigner_ControlAccess/DesignerForm1.cs
@@ -1,4 +1,6 @@
 ﻿using DevExpress.XtraCharts;
+using DevExpress.XtraGauges.Core.Drawing;
+using DevExpress.XtraGauges.Win.Gauges.Circular;
 using DevExpress.XtraGrid.Views.Grid;
 using DevExpress.XtraPivotGrid;
 using System.Drawing;
@@ -14,7 +16,7 @@ namespace DashboardDesigner_ControlAccess
         }
 
         private void dashboardDesigner_DashboardItemControlCreated(object sender, DevExpress.DashboardWin.DashboardItemControlEventArgs e) {
-            if(e.DashboardItemName == "pivotDashboardItem1") {
+            if (e.PivotGridControl != null) {
                 PivotGridControl pivotGridControl = e.PivotGridControl;
                 pivotGridControl.CustomCellValue += PivotGridControl_CustomCellValue; ;
             }
@@ -27,18 +29,25 @@ namespace DashboardDesigner_ControlAccess
         }
 
         private void dashboardDesigner_DashboardItemControlUpdated(object sender, DevExpress.DashboardWin.DashboardItemControlEventArgs e) {
-            if(e.DashboardItemName == "gridDashboardItem1") {
+            if(e.GridControl!= null) {
                 GridView gridView = e.GridControl.MainView as GridView;
                 gridView.Appearance.Row.Font = new Font("Arial", 10);
             }
             if(e.DashboardItemName == "chartDashboardItem1") {
                 ChartControl chartControl = e.ChartControl;
-                ((XYDiagram)chartControl.Diagram).Panes[0].BackColor = Color.AliceBlue;
+                ((XYDiagram)chartControl.Diagram).Panes[0].BackColor = Color.LightYellow;
+            }
+            if (e.DashboardItemName == "gaugeDashboardItem1") {
+                var gauge = e.GaugeControl.Gauges[0] as CircularGauge;
+                gauge.Labels[0].AppearanceBackground.ContentBrush = new SolidBrushObject(Color.LightGreen);
+                gauge.Labels[0].Text = "Custom Revenue Label";
+                gauge.Labels[0].AppearanceText.Font = new Font("Tahoma", 16);
+                gauge.Scales[0].MajorTickCount = 8;
             }
         }
 
         private void dashboardDesigner_DashboardItemBeforeControlDisposed(object sender, DevExpress.DashboardWin.DashboardItemControlEventArgs e) {
-            if(e.DashboardItemName == "pivotDashboardItem1") {
+            if(e.PivotGridControl != null) {
                 PivotGridControl pivotGridControl = e.PivotGridControl;
                 pivotGridControl.CustomCellValue -= PivotGridControl_CustomCellValue;
             }
